2801f0280cc52c0e6563081478b09036880d78e7
Derek Adair <d@derekadair.com>
2021-10-01 15:29:49 +0000

    creating/commiting to a new git repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,7 @@
 docker-compose run --rm web django-admin startproject $1 . \
     && docker-compose run --rm web python ./manage.py migrate \
-    && docker-compose run --rm web sudo chown -R $(whoami):$(whoami)
+    && sudo chown -R $(whoami):$(whoami) . \
+    && rm -rf .git \
+    && git init \
+    && git add . \
+    && git commit -m "Bootstrapped $1"
